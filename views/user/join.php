85b48d22cdc4890c9f3572fa44d44585491c2cfb
artem <programmist.nazarov@gmail.com>
2022-11-11 17:18:11 +0500

    user view (ui)

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,14 @@
-<h1>Форма для регистрации</h1>
+<h1>Форма для логина</h1>
+<div class="panel panel-info">
+    <div class='panel-heading'></div><!-- comment -->
+</div class='panel-body'>
+
+Имя пользователя_<br/><!-- comment -->
+Логин пользователя_<br/><!-- comment -->
+Пароль 1_<br/><!-- comment -->
+Подтверждение пароля_<br/><!-- comment -->
+[КНОПКА ОТПРАВИТЬ]<br/><!-- comment -->
+
+
+</div>
+</div>
--- views/user/login.php
+++ views/user/login.php
@@ -1,1 +1,12 @@
-<h1>Форма для логина</h1>
+<h1>Форма для входа</h1>
+<div class="panel panel-info">
+    <div class='panel-heading'></div><!-- comment -->
+<div class='panel-body'>
+
+Логин пользователя_<br/><!-- comment -->
+Пароль_<br/><!-- comment -->
+[КНОПКА ВОЙТИ]<br/><!-- comment -->
+
+
+</div>
+</div>
